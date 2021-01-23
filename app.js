8209946f4dd335cc072ad6127289d7a0aae79698
Clinton Onwu <clintononwu@yahoo.com>
2021-01-23 11:56:32 -0600

    multiple - Set up user models, controllers and route (signup, login, logout, details) Set up validation for user routes Set up authentication, db initialization Set up api 404 and error handlers

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
+// express generator defaults
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// other middleware required
+const compression = require('compression');
+const helmet = require('helmet');
 
+const initializeAuth = require('./lib/auth/authentication');
+const {errorHandler, noPageHandler} = require('./lib/helpers/route-errors');
+
+// Routes
+const sendReactApp = require('./lib/routes/send-react-app');
+const apiRouter = require('./lib/routes/api');
+
+// Initialize app
 const app = express();
 
+// env configuration if not in Production or Testing
+if (app.get('env') === 'development') require('dotenv').config();
+
+// Initialize db connection
+const db = require('./lib/db/db_index');
+
+// Initialize session and authentication
+initializeAuth(app, db);
+
+// Initialize pre-routing middleware
+app.use(compression());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// routing
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRouter);
+app.use('/', sendReactApp);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// 404 handler
+app.use(noPageHandler);
 
+// error handler
+app.use(errorHandler);
+
+// export to bin/www
 module.exports = app;
